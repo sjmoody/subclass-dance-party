50b51a7684a2b22237628c7752630ce8c23b1433
Steven Moody <smoody07@gmail.com>
2022-06-23 21:08:31 -0700

    Complete adding css

---
+++ src/boatDancer.js
@@ -0,0 +1,40 @@
+var BoatDancer = function(top, left, time) {
+  BlinkyDancer.call(this, top, left, time);
+  console.log(this.left);
+  this.left = left;
+  this.top = top;
+  this.time= time;
+};
+BoatDancer.prototype = Object.create(BlinkyDancer.prototype);
+BoatDancer.prototype.constructor = BoatDancer;
+
+BoatDancer.prototype.step = function() {
+  Dancer.prototype.step.call(this);
+  // move left to right
+  //https://api.jquery.com/category/effects/
+  // console.log(this.$node);
+  // console.log("this is ");
+  // console.log(this);
+  // console.log(this.left)
+  this.left = Math.random() * $("body").width();
+  this.top = Math.random() * $("body").height();
+
+  // var styleSettings = {
+  //   top: this.top,
+  //   left: this.left
+  // };
+  // this.color = '#' + Math.floor(Math.random()*16777215).toString(16);
+  // // console.log(this.color);
+  this.$node.css('border', 'solid 10px  white');
+  this.$node.animate({
+    top: this.top,
+    left: this.left
+  })
+  // #e19219
+  //   const randomColor = '#'+ Math.floor(Math.random()*16777215).toString(16);
+  //document.body.style.backgroundColor = "#" + randomColor;
+  // color.innerHTML = "#" + randomColor;
+
+
+
+};
